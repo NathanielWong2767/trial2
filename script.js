76dc370b0eabaf8a92b4e314d26398da09eff9b1
NathanielWong2767 <rolandnathaniel21@gmail.com>
2024-06-19 10:47:14 +0800

    gg

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const inputElement = document.getElementById('input')
 
-let notEmpty = inputElement.value
+let notEmpty = undefined
 
 const buttonHello = () => {
     console.log(inputElement.value)
+    console.log(typeof (notEmpty))
+    console.log(typeof (inputElement.value))
 
-    if (notEmpty != null ) {
+    if (notEmpty != undefined ) {
     document.getElementById('title').style.color = "rgb(238, 207, 30)";
     }
  }
